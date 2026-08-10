d4bd75c816599c0ed334a31b52f46900e8b3c1d0
sahilnabi279 <sahilnabi279@gmail.com>
2026-08-10 17:38:04 +0530

    Organize Week 1 and Week 2 assignments

---
+++ Week1/Celsius to fahrenheit.py
@@ -0,0 +1,7 @@
+print("Weather AI Temperature Converter ")
+
+celsius = float(input("Enter temperature in Celsius: "))
+
+fahrenheit = (celsius * 9/5) + 32
+
+print("Temperature in Fahrenheit =", fahrenheit)
